cd1cf6135b7cbed91a50b32b9b9e7a5259dae95d
Teekung123 <teehid099@gmail.com>
2025-01-31 17:15:20 +0700

    first commit

--- app.js
+++ app.js
@@ -1,22 +1,30 @@
-const express = require("express");
-const app = express();
-const path = require('path');
+const express = require("express"); // ใช้สร้างเซิร์ฟเวอร์
+const bodyParser = require('body-parser');// แปลงข้อมูลจากฟอร์มหรือ JSON ที่มาจากฝั่ง client ให้ใช้งานได้ง่ายใน req.body.
+const path = require('path');// ใช้จัดการ path ของไฟล์หรือโฟลเดอร์ในระบบปฏิบัติการ
+const { render } = require("ejs");
 
-let dir = path.join(__dirname, '../../')
-let url = path.resolve(__dirname, '../../')
+const app = express(); //ตัวแปรที่ใช้กำหนดการตั้งค่าต่าง ๆ และจัดการเส้นทางของเซิร์ฟเวอร์.
+const port = 3000; //ระบุพอร์ตที่เซิร์ฟเวอร์จะรัน
 
-app.use(express.static(dir));
+app.set('views',`${__dirname}/static/member`); // กำหนดตำแหน่งโฟลเดอร์ที่เก็บไฟล์ Views (template) เป็นโฟลเดอร์ static
+app.set('view engine','ejs'); // ตั้งค่าให้ใช้ EJS เป็น template engine
 
-const PORT = 3000;
-app.listen(PORT, () =>{
-    console.log(`${PORT}`);
+
+// กำหนดให้โฟลเดอร์ static เป็นโฟลเดอร์สำหรับเสิร์ฟไฟล์ Static เช่น CSS, JavaScript, และรูปภาพ.
+let root_path = path.resolve(__dirname,'static');
+
+app.use(express.static(root_path));
+
+app.get('/login',(req,res) => {
+    res.render('sing in');
 });
 
-app.get('/',(req, res) =>{
-    res,sendFile(url)
-});
+app.get('/home',(req,res) => {
+    res.render('home')
+})
 
-app.get('/',(req,res)=>{
-    res.setHeader('','')
-    res.send('')
-});
+
+
+app.listen(port,() =>{
+    console.log(`Server is runing>>> http://localhost:${port}/login`)
+})

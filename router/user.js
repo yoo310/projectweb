eac319a1268084bf8c72d07464f0a48c8147c11b
Teekung123 <teehid099@gmail.com>
2025-02-02 22:58:01 +0700

    first commit

--- router/user.js
+++ router/user.js
@@ -53,4 +53,41 @@ router.post('/verify',(req,res) => {
 
 });
 
+const storage = multer.memoryStorage(); // ใช้ memoryStorage สำหรับเก็บไฟล์ใน RAM ชั่วคราว
+const upload = multer({ storage });
+
+router.post("/create_post",upload.single("image"), (req, res) => {
+     // ตรวจสอบค่าทั้งหมดที่ได้รับ
+    const username = req.session.username
+
+    if (!username){
+        return res.send("User not logged in");
+    }
+    
+    const sql = "SELECT ID,username FROM member WHERE username = ? "
+    pool.query(sql,[username],(err,results) =>{
+        if (err){
+            console.log("Data Base error !!")
+            return res.send("Data Base error !!")
+        }if (results.length === 0) {
+            return res.status(404).json({ error: "User not found" });
+        }
+        const memberID = results[0].ID
+        const member_name = results[0].username
+        const fileBuffer = req.file.buffer; // ดึงข้อมูลไฟล์จาก RAM
+        const caption = req.body.caption;
+
+        const insertSql  = "INSERT INTO post (img,capion,memberID,memberName) VALUES (?,?,?,?)";
+        pool.query(insertSql , [fileBuffer,caption,memberID,member_name], (err, result) => {
+            if (err) {
+                console.error("Error saving to database:", err);
+                return res.status(500).send("Database error");
+            }
+            res.render("home", { username: username });
+            // res.send({ message: "File uploaded successfully"});
+        });
+    });
+    
+});
+
 module.exports = router;
